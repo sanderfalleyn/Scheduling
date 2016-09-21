f43835416bd50d2eef6d52246119e7523510903b
DESKTOP-AOU0QSI\Sander Falleyn <sander.falleyn@gmail.com>
2016-09-21 13:53:13 +0200

    FEAT: install hangfirenuget FEAT: startup

---
+++ Webapplication/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using Hangfire;
+using Microsoft.Owin;
+using Owin;
+using Webapplication;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace Webapplication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["hangfire"].ConnectionString;
+            GlobalConfiguration.Configuration
+                .UseSqlServerStorage(connectionString);
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
